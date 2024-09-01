754ad43fde57ebaa6a0f351850ec1be6bdfbbdb9
Kosta-111 <nestt@ukr.net>
2024-09-01 23:43:20 +0300

    Add project files.

---
+++ OnlineCourses/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OnlineCourses.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
